6ddda99e71565d71362b96d16bf23a2ef2e94a4a
rjgamer <jonathan.nessier@outlook.com>
2016-11-10 13:00:39 +0100

    Optimized author param

--- info.php
+++ info.php
@@ -15,7 +15,7 @@ $template_name = 'Fraggy Backend Theme';
 $template_function = 'theme';
 $template_version = '1.5.2';
 $template_platform = '1.1.x';
-$template_author = 'Jonathan Nessier - <a href="https://www.neoflow.ch" target="_blank">Neoflow</a>';
+$template_author = 'Jonathan Nessier, <a href="https://www.neoflow.ch" target="_blank">Neoflow</a>';
 $template_license = '<a href="https://github.com/Neoflow/Fraggy-Backend-Theme/blob/master/LICENSE" target="_blank">MIT License</a>';
 $template_license_terms = '-';
 $template_description = 'Responsive and Bootstrap based backend theme for WBCE';
