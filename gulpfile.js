687999d5e635a9e84064bc6b24d045f0c35097a6
rjgamer <jonathan.nessier@neoflow.ch>
2016-11-10 12:39:31 +0100

    Optimized source handling

--- gulpfile.js
+++ gulpfile.js
@@ -6,40 +6,57 @@ var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var zip = require('gulp-zip');
-
+var replace = require('gulp-replace');
+var saveLicense = require('uglify-save-license');
 
 var pjson = require('./package.json');
 
-gulp.task('default', function () {
-    gulp.start('sass:watch');
-    gulp.start('css:watch');
+gulp.task('build-scss', function () {
+    return gulp.src('./src/sass/**/*.scss')
+            .pipe(sass({
+                outputStyle: 'expanded'
+            }).on('error', sass.logError))
+            .pipe(replace(/(\@charset\s\"UTF\-8\"\;[\r\n]+)/igm, ''))
+            .pipe(gulp.dest('./css'));
 });
 
-gulp.task('sass', function () {
-    return gulp.src('./css/src/sass/**/*.scss')
-            .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-            .pipe(gulp.dest('./css/src'));
+gulp.task('minify-css', function () {
+    return gulp.src(['./css/*.css', '!./css/*.min.css'])
+            .pipe(cleanCSS())
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            //.pipe(replace('*/', '*/\r\n'))
+            // .pipe(replace('/*!', '\r\n\r\n/*!'))
+            .pipe(gulp.dest('./css'));
+});
+
+gulp.task('build-js', function () {
+    return gulp.src([
+        './src/js/vendor/**/*.js',
+        './src/js/theme/*.js'])
+            .pipe(concat('script.js'))
+            .pipe(gulp.dest('./js'));
 });
 
 gulp.task('minify-js', function () {
-    return gulp.src(['./js/src/*.js'])
-            .pipe(uglify())
+    return gulp.src([
+        './js/script.js',
+        '!./js/script.min.js'])
+            .pipe(uglify({
+                output: {
+                    comments: saveLicense
+                }
+            }))
             .pipe(rename({suffix: '.min'}))
             .pipe(gulp.dest('./js'));
 });
 
-gulp.task('minify-css', function () {
-    return gulp.src(['./css/src/*.css'])
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(gulp.dest('./css'));
-});
-
 gulp.task('watch', function () {
-    gulp.watch('./css/src/sass/**/*.scss', ['sass']);
-    gulp.watch('./css/src/*.css', ['minify-css']);
-    gulp.watch('./js/src/*.js', ['minify-js']);
+    gulp.watch(['./src/sass/**/*.scss'], ['build-scss']);
+    gulp.watch(['./css/style.css'], ['minify-css']);
+    gulp.watch(['./src/js/**/*.js'], ['build-js']);
+    gulp.watch(['./js/script.js'], ['minify-js']);
 });
 
 gulp.task('zip', function () {
