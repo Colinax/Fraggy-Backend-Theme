94012ccd70bfb84cd6b23542258f4abef53ec3d6
rjgamer <jonathan.nessier@neoflow.ch>
2017-09-17 09:34:36 +0200

    Added datetime picker

--- js/script.js
+++ js/script.js
@@ -3142,5 +3142,14 @@ $(function () {
 });
 $(function () {
 
+    $('.datetimepicker').each(function () {
+        var $this = $(this);
+        $this.datetimepicker({
+            locale: $this.data('language') || LANGUAGE_CODE,
+            format: $this.data('format') ? $this.data('format') : 'DD.MM.Y HH:mm',
+            extraFormats: ['M/D/Y HH:mm'],
+            useCurrent: false
+        });
+    });
 
 });
--- src/js/theme/init/datetimepicker.js
+++ src/js/theme/init/datetimepicker.js
@@ -1,14 +1,14 @@
 // Initialize Bootstrap Datetimepicker
 $(function () {
 
-//    $('.datetimepicker').each(function () {
-//        var $this = $(this);
-//        $this.datetimepicker({
-//            locale: $this.data('language') || LANGUAGE_CODE,
-//            format: $this.data('format') ? $this.data('format') : 'DD.MM.Y HH:mm',
-//            extraFormats: ['M/D/Y HH:mm'],
-//            useCurrent: false
-//        });
-//    });
+    $('.datetimepicker').each(function () {
+        var $this = $(this);
+        $this.datetimepicker({
+            locale: $this.data('language') || LANGUAGE_CODE,
+            format: $this.data('format') ? $this.data('format') : 'DD.MM.Y HH:mm',
+            extraFormats: ['M/D/Y HH:mm'],
+            useCurrent: false
+        });
+    });
 
 });
