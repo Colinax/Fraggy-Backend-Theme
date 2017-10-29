cdaf778e92b44694e11cc0ffe11ca12579720904
rjgamer <jonathan.nessier@neoflow.ch>
2017-10-29 14:49:53 +0100

    Updated version

--- info.php
+++ info.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Fraggy Backend Theme
  * Responsive and Bootstrap based backend theme for WBCE
@@ -13,8 +14,8 @@ if (!defined('WB_PATH')) {
 $template_directory = 'fraggy-backend-theme';
 $template_name = 'Fraggy Backend Theme';
 $template_function = 'theme';
-$template_version = '2.0.0-beta3';
-$template_platform = '1.2.x';
+$template_version = '2.0.0-betaç';
+$template_platform = '1.3.x';
 $template_author = '2016-2017 Jonathan Nessier, <a href="https://www.neoflow.ch" target="_blank">Neoflow</a>';
 $template_license = '<a href="https://github.com/Neoflow/Fraggy-Backend-Theme/blob/master/LICENSE" target="_blank">MIT License</a>';
 $template_license_terms = '-';
