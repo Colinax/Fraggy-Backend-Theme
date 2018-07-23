ae2a3f328e9696a93417baa677d77afa89256af4
rjgamer <jonathan.nessier@neoflow.ch>
2018-07-23 14:26:42 +0200

    Rewrite copyright mark

--- languages/index.php
+++ languages/index.php
@@ -3,7 +3,7 @@
  * Fraggy Backend Theme
  * Responsive and Bootstrap based backend theme for WBCE
  *
- * @copyright 2016-2018 Jonathan Nessier, Neoflow | neoflow.ch
+ * @copyright 2016-2018 Jonathan Nessier, Neoflow <jonathan.nessier@neoflow.ch>
  * @license GNU GPLv3
  */
 header('Location: ../../../index.php');
