71c7fbb142bbeb4bacf8ed982d6cfaa78759d700
rjgamer <jonathan.nessier@neoflow.ch>
2019-07-15 14:04:20 +0200

    Fixed #36

--- src/js/theme/init/select2.js
+++ src/js/theme/init/select2.js
@@ -4,30 +4,30 @@ $.getScript(THEME_URL + '/js/select2/i18n/' + LANGUAGE_CODE + '.js');
 // Initialize Select2
 $(function () {
     $('select')
-            .select2({
-                theme: 'bootstrap',
-                //  minimumResultsForSearch: -1,
-                width: '100%',
-                dropdownAutoWidth: true,
-                templateResult: function (item) {
-                    if (item.hasOwnProperty('element')) {
-                        var $element = $(item.element);
-                        if ($element.data('level')) {
-                            var level = $element.data('level');
-                            return $('<span style="padding-left:' + (20 * parseInt(level)) + 'px;">' + item.text + '</span>');
-                        } else if ($element.data('description')) {
-                            var description = $element.data('description');
-                            return $('<span>' + item.text + '</span><br /><small>' + description + '</small>');
-                        }
+        .select2({
+            theme: 'bootstrap',
+            //  minimumResultsForSearch: -1,
+            width: '100%',
+            dropdownAutoWidth: false,
+            templateResult: function (item) {
+                if (item.hasOwnProperty('element')) {
+                    var $element = $(item.element);
+                    if ($element.data('level')) {
+                        var level = $element.data('level');
+                        return $('<span style="padding-left:' + (20 * parseInt(level)) + 'px;">' + item.text + '</span>');
+                    } else if ($element.data('description')) {
+                        var description = $element.data('description');
+                        return $('<span>' + item.text + '</span><br /><small>' + description + '</small>');
                     }
-                    return item.text;
                 }
-            })
-            .on('select2:ready change', function () {
-                $(this).find('+ .select2 .select2-selection__choice__remove').empty();
-            })
-            .trigger('select2:ready')
-            .focus(function () {
-                $(this).select2('open');
-            });
+                return item.text;
+            }
+        })
+        .on('select2:ready change', function () {
+            $(this).find('+ .select2 .select2-selection__choice__remove').empty();
+        })
+        .trigger('select2:ready')
+        .focus(function () {
+            $(this).select2('open');
+        });
 });
