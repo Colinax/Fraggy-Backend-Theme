101aede23fbab6c60740779d1cf5b5d7621aa5cd
rjgamer <jonathan.nessier@neoflow.ch>
2019-07-16 20:28:32 +0200

    Updated copyright year

--- languages/EN.php
+++ languages/EN.php
@@ -3,7 +3,7 @@
  * Fraggy Backend Theme
  * Responsive and Bootstrap based backend theme for WBCE
  *
- * @copyright 2016-2018 Jonathan Nessier, Neoflow <jonathan.nessier@neoflow.ch>
+ * @copyright 2016-2019 Jonathan Nessier, Neoflow <jonathan.nessier@neoflow.ch>
  * @license GNU GPLv3
  */
 $template_description = 'Responsive and Bootstrap based backend theme for WBCE';
